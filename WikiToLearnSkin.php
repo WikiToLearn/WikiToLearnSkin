974fc23f769ebb6dac08f9e85dce18a83571dd08
Gianluca Rigoletti <gianluca.rigoletti@gmail.com>
2016-08-30 17:53:12 +0200

    Add entrypoint WikiToLearnSkin.php

---
+++ WikiToLearnSkin.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Skin file for skin Foo Bar.
+ *
+ * @file
+ * @ingroup Skins
+ */
+
+/**
+ * SkinTemplate class for Foo Bar skin
+ * @ingroup Skins
+ */
+class SkinWikiToLearnSkin extends SkinTemplate {
+    var $skinname = 'wikitolearnskin', $stylename = 'WikiToLearnSkin',
+        $template = 'WikiToLearnSkinTemplate', $useHeadElement = true;
+
+    /**
+     * This function adds JavaScript via ResourceLoader
+     *
+     * Use this function if your skin has a JS file or files.
+     * Otherwise you won't need this function and you can safely delete it.
+     *
+     * @param OutputPage $out
+     */
+    
+    public function initPage( OutputPage $out ) {
+        parent::initPage( $out );
+        $out->addModules( 'skin.wikitolearn.js' );
+    }
+
+    /**
+     * Add CSS via ResourceLoader
+     *
+     * @param $out OutputPage
+     */
+    function setupSkinUserCss( OutputPage $out ) {
+        parent::setupSkinUserCss( $out );
+        $out->addModuleStyles( 'skin.wikitolearn' );
+    }
+}
+
+/**
+ * BaseTemplate class for Foo Bar skin
+ *
+ * @ingroup Skins
+ */
+class WikiToLearnSkinTemplate extends BaseTemplate {
+    /**
+     * Outputs the entire contents of the page
+     */
+    public function execute() {
+        $this->html( 'headelement' ); ?>
+
+/** Your skin's code goes here.
+  * The rest of this page explains what to write here.
+**/
+
+<?php $this->printTrail(); ?>
+</body>
+</html><?php
+    }
+}
