3bb24563d4094773fe7d4c6ed471faf10fa88583
Cristian Baldi <bld.cris.96@gmail.com>
2016-09-23 14:07:50 +0200

    Fix on small pages

--- scripts/floatingTools.js
+++ scripts/floatingTools.js
@@ -4,15 +4,17 @@ $( document ).ready(function() {
 
     toolsContainer = $("#tools_container");
 
-    articleMain = $(".article__main");
+    articleMain = $(".article__sheet");
 
-    $(document).scroll(function() {
-        toolsContainer.css("position", "fixed");
-        toolsContainer.css("bottom", "");
+    if(articleMain.height() > toolsContainer.height()){
+        $(document).scroll(function() {
+            toolsContainer.css("position", "fixed");
+            toolsContainer.css("bottom", "");
 
-        if(toolsContainer.offset().top + toolsContainer.height() > articleMain.offset().top + articleMain.height()){
-            toolsContainer.css("position", "absolute");
-            toolsContainer.css("bottom", "0");
-        }
-    });
+            if(toolsContainer.offset().top + toolsContainer.height() > articleMain.offset().top + articleMain.height()){
+                toolsContainer.css("position", "absolute");
+                toolsContainer.css("bottom", "0");
+            }
+        });
+    }
 });
