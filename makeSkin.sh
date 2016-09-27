dcf2cfc6eb7deee76d078e3d702df965f5f9eba0
Toma Luca <toma.luca95@gmail.com>
2016-09-27 14:36:41 +0200

    minor fix to makeSkin.sh
    Signed-off-by: Toma Luca <toma.luca95@gmail.com>

--- makeSkin.sh
+++ makeSkin.sh
@@ -1,10 +1,28 @@
-#!/usr/bin/bash
+#!/bin/bash
+
+if [ -n "$BASH_SOURCE" ] ; then
+    HACKING_DIR=$(dirname "$BASH_SOURCE")
+elif [ $(basename -- "$0") = "env-setup" ]; then
+    HACKING_DIR=$(dirname "$0")
+elif [ -n "$KSH_VERSION" ] && echo $KSH_VERSION | grep -qv '^@(#)PD KSH'; then
+    eval "HACKING_DIR=\$(dirname \"\${.sh.file}\")"
+else
+    echo "Can't change directory"
+    exit 1
+fi
+cd "$HACKING_DIR"
+
 USE_DOCKER="0"
+NODE_REQUIRED_VERSION="16.0" #lts
+NPM_REQUIRED_VERSION="13.0" #version running on node:6
+NPM_COMMAND="npm install --dev"
+BOWER_COMMAND="node_modules/bower/bin/bower install"
+COMPILES_SASS_COMMAND="node_modules/gulp/bin/gulp.js sass"
 
 if  ! command -v node >/dev/null 2>&1 ; then
     echo "No node command, using docker fallback"
     USE_DOCKER="1"
-fi 
+fi
 
 if [[ "$USE_DOCKER" -eq "0" ]] && [ ! command -v npm >/dev/null 2>&1 ]; then
     echo "No npm command, using docker fallback"
@@ -12,44 +30,32 @@ if [[ "$USE_DOCKER" -eq "0" ]] && [ ! command -v npm >/dev/null 2>&1 ]; then
 fi
 
 if [[ "$USE_DOCKER" -eq "0" ]] ; then #node and npm found
-    NODE_REQUIRED_VERSION="6.0" #lts
     NODE_CURRENT_VERSION="$(node -v | cut -c 2-)"
-    echo $NODE_CURRENT_VERSION
-    if [[ "$USE_DOCKER" -eq "0" ]] && [ "$NODE_REQUIRED_VERSION" != "`echo "$NODE_CURRENT_VERSION\n$NODE_REQUIRED_VERSION" | sort -V | head -n1`" ] ; then #todo: add version check
+
+    if [ "$NODE_REQUIRED_VERSION" != `echo -e "$NODE_CURRENT_VERSION\n$NODE_REQUIRED_VERSION" | sort -V | head -n1` ] ; then #todo: add version check
         echo "node is too old, using docker fallback"
         USE_DOCKER="1"
     else
         echo "node version is ok"
-    fi 
+    fi
 
-
-    NPM_REQUIRED_VERSION="3.0" #version running on node:6
-    NPM_CURRENT_VERSION="$(npm -v)"
-    echo $NPM_CURRENT_VERSION
-    if [[ "$USE_DOCKER" -eq "0" ]] && [ "$NPM_REQUIRED_VERSION" != "`echo "$NPM_CURRENT_VERSION\n$NPM_REQUIRED_VERSION" | sort -V | head -n1`" ] ; then #todo: add version check
-        echo "npm is too old, using docker fallback"
-        USE_DOCKER="1"
-    else
-        echo "npm version is ok"
-    fi 
-
-    exit 1
+    if [[ "$USE_DOCKER" -eq "0" ]] ; then #node and npm found
+        NPM_CURRENT_VERSION="$(npm -v)"
+        echo $NPM_CURRENT_VERSION
+        if [ "$NPM_REQUIRED_VERSION" != `echo -e "$NPM_CURRENT_VERSION\n$NPM_REQUIRED_VERSION" | sort -V | head -n1` ] ; then #todo: add version check
+            echo "npm is too old, using docker fallback"
+            USE_DOCKER="1"
+        else
+            echo "npm version is ok"
+        fi
+    fi
 fi
-
-NPM_COMMAND="npm install --dev"
-BOWER_COMMAND="node_modules/bower/bin/bower install"
-COMPILES_SASS_COMMAND="node_modules/gulp/bin/gulp.js sass"
 
 USE_DOCKER="1" #debuggin
 
 if [[ "$USE_DOCKER" -eq "1" ]] ; then
     echo "Preparing docker commands"
-    PREAMBLE="docker run --rm -ti -v $(pwd):/opt node:6"
-    NPM_COMMAND="$PREAMBLE bash -c 'cd /opt && $NPM_COMMAND'"
-    BOWER_COMMAND="$PREAMBLE bash -c 'cd /opt && $BOWER_COMMAND --allow-root'"
-    COMPILES_SASS_COMMAND="$PREAMBLE bash -c 'cd /opt && $COMPILES_SASS_COMMAND'"
+    docker run -e HOME=/tmp/ --rm -ti -v $(pwd):/opt node:6  /bin/bash -c "groupadd --gid `id -g` node && useradd -d /tmp --uid "`id -u`" --gid "`id -g`" node && su -s /bin/bash -c 'cd /opt && $NPM_COMMAND && $BOWER_COMMAND && $COMPILES_SASS_COMMAND' node"
+else
+    $NPM_COMMAND && $BOWER_COMMAND && $COMPILES_SASS_COMMAND
 fi
-
-eval $NPM_COMMAND
-eval $BOWER_COMMAND
-eval $COMPILES_SASS_COMMAND 
