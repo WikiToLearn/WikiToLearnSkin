30ad0cb47df44da63049257ba6909d8d67fd964e
Cristian Baldi <bld.cris.96@gmail.com>
2016-10-05 21:17:02 +0200

    Remove debug console.log

--- scripts/search.js
+++ scripts/search.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
-    console.log("here");
     $('#searchForm').submit(function(e) {
         e.preventDefault();
         var len = $('#searchInput').val().length;
         if(len > 0)
             this.submit();
     });
-
 });
