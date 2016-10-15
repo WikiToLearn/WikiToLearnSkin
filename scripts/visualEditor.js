df30523b448cb3144790f35c2e955644a1e8c97c
Cristian Baldi <bld.cris.96@gmail.com>
2016-10-15 14:26:42 +0200

    Add proper check if ve is open

--- scripts/visualEditor.js
+++ scripts/visualEditor.js
@@ -7,13 +7,13 @@ mw.hook( 've.activationComplete' ).add( function () {
     $("#ca-ve-edit").attr("title", mw.message( 'view' ).text());
 
     $("#ca-ve-edit").click(function(e){ //because somehow the clicks on links are locked
-        if($(this).hasClass("inVisualEditor"))
+        if(window.ve && ve.init && ve.init.target && ve.init.target.active) //proper way to check if VE is open
             location.href = location.href.split("?")[0];
     });
 });
 
+//handle back button pressed
 mw.hook( 've.deactivate' ).add( function () {
-    $("#ca-ve-edit").removeClass("inVisualEditor");
     $("#ca-ve-edit").attr("href", location.href + "?veaction=edit");
     $("#ca-ve-edit").find(".tool__icon").removeClass("fa-eye").addClass("fa-pencil");
     $("#ca-ve-edit").find(".tool__title").text(mw.message( 'edit' ).text());
