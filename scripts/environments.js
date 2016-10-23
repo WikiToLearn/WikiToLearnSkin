f2652052ac0cabc00b230795c515636e1aad2002
Cristian Baldi <bld.cris.96@gmail.com>
2016-10-23 13:59:11 +0200

    Improve the environments script, now support pure text nodes

--- scripts/environments.js
+++ scripts/environments.js
@@ -1,12 +1,22 @@
 //I'm sorry, I have not found a better way to do this.
+//Thanks: http://stackoverflow.com/questions/3276133/jquery-wrapping-text-and-elements-between-hr-tags
 $(document).ready(function(){
     $(".beginenvironment").each(function() {
         classes = $(this).attr('class').trim();
         classes = "." + classes;
         classes = classes.replace(" ", ".");
         replaced = classes.replace('.beginenvironment','').replace('begin','end').trim();
-        console.log(replaced);
-        if(replaced.length > 0)
-            $(this).nextUntil(replaced).wrapAll('<div class="environmentcontent"></div>');
+        if(replaced.length > 0){
+            var $set = $();
+            var nxt = this.nextSibling;
+            console.log(nxt);
+            while(nxt) {
+                if(!$(nxt).is(replaced)) {
+                    $set.push(nxt);
+                    nxt = nxt.nextSibling;
+                } else break;
+            }
+            $set.wrapAll('<div class="environmentcontent" />');
+        }
     });
 });
