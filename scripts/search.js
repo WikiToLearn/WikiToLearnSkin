90a45beaac7f49ad7343d3e4b5e7e1eec0cd8fba
Cristian Baldi <bld.cris.96@gmail.com>
2016-10-28 20:49:19 +0200

    Fix T4185: Pressing on the search bar icon does not focus the search field

--- scripts/search.js
+++ scripts/search.js
@@ -2,7 +2,11 @@ $(document).ready(function() {
     $('#searchForm').submit(function(e) {
         e.preventDefault();
         var len = $('#searchInput').val().length;
-        if(len > 0)
+        if(len > 0){
             this.submit();
+        } else {
+            $('#searchInput').focus();
+            $('#searchInput').attr('class', 'expanded-search');
+        }
     });
 });
